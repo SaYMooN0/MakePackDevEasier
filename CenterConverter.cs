3b9ada13bab0d7825b88c715da366ffd31cd1e66
SaYMooN <shokoladnysaymoon@mail.ru>
2023-03-12 10:38:00 +0300

    Starting of something big

---
+++ CenterConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MDE
+{
+    public class CenterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double size = (double)value;
+            double labelSize = double.Parse(parameter.ToString());
+            return (size - labelSize) / 2;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
